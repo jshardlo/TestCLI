9173ad35b7f04ebfd75fe356582dc260f2016256
John Shardlow <john@shardlow.org.uk>
2019-02-12 14:56:18 +0800

    Initial Commit

---
+++ TestCLI/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TestCLI
+//
+//  Created by John Shardlow on 12/02/2019.
+//  Copyright © 2019 John Shardlow. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
