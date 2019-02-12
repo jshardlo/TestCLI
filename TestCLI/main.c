d44070eb9676e895faefd106715a361570f91165
John Shardlow <john@shardlow.org.uk>
2019-02-12 15:01:15 +0800

    Added args display

--- TestCLI/main.c
+++ TestCLI/main.c
@@ -10,6 +10,12 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
+    printf("Number of args: %d\n", argc);
+    int c;
+    
+    for (c=0; c<argc; c++) {
+        printf("%s\n", argv[c]);
+    }
     printf("Hello, World!\n");
     return 0;
 }
